cde993eded449491428a7b29d3678b2abee1d233
huehue <58138952+huehue95@users.noreply.github.com>
2022-05-14 21:44:52 +0300

    pieni secure

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -12,19 +12,35 @@ end)
 
 RegisterNetEvent('kiitiautost')
 AddEventHandler('kiitiautost', function()
-	local v = Config.models[math.random(1, #Config.models)]
-	RequestModel(v)
-	while not HasModelLoaded(v) do
-		Citizen.Wait(10)
-	end
-	local p = PlayerPedId()
-	local pos = GetEntityCoords(p)
-	local veh = CreateVehicle(v, pos.x, pos.y, pos.z, GetEntityHeading(p), true, false)
-	local newPlate     = exports.esx_vehicleshop:GeneratePlate()
-	local vehicleProps = ESX.Game.GetVehicleProperties(veh)
-	--local vehicleProps = exports.renzu_customs:GetVehicleProperties(veh)
-	vehicleProps.plate = newPlate
-	TaskWarpPedIntoVehicle(p, veh, -1)
-	SetVehicleNumberPlateText(veh, newPlate)
-	TriggerServerEvent('bottle:setVehicleOwned', vehicleProps)
+    if getItems("avain")then
+        local v = Config.models[math.random(1, #Config.models)]
+        RequestModel(v)
+        while not HasModelLoaded(v) do
+            Citizen.Wait(10)
+        end
+        local p = PlayerPedId()
+        local pos = GetEntityCoords(p)
+        local veh = CreateVehicle(v, pos.x, pos.y, pos.z, GetEntityHeading(p), true, false)
+        local newPlate     = exports.esx_vehicleshop:GeneratePlate()
+        local vehicleProps = ESX.Game.GetVehicleProperties(veh)
+        vehicleProps.plate = newPlate
+        TaskWarpPedIntoVehicle(p, veh, -1)
+        SetVehicleNumberPlateText(veh, newPlate)
+        TriggerServerEvent('bottle:setVehicleOwned', vehicleProps)
+    else
+        TriggerServerEvent('bottle:eiitemiä')
+    end
 end)
+
+
+function getItems(item)
+    local Inventory = ESX.GetPlayerData().inventory
+    for i=1, #Inventory, 1 do
+        if Inventory[i].name == item then
+            if Inventory[i].count and Inventory[i].count > 0 then
+                return true
+            end
+        end
+    end
+    return false
+end
